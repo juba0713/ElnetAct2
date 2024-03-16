1c566a205f4cc92e94594021fc535213fe2014bf
juba0713 <basasrepo@gmail.com>
2024-03-16 09:08:04 +0800

    Add project files.

---
+++ ElnetAct2/Controllers/CineEase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElnetAct2.Controllers
+{
+    public class CineEase : Controller
+    {
+   
+        public IActionResult Home()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ ElnetAct2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElnetAct2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
